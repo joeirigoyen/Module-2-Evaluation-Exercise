bef2060f6320f50f8055a7341e0906aed1a8e0cb
Youthan Irigoyen <a01750476@itesm.mx>
2022-08-28 23:53:49 -0500

    Fixed file structure.

--- processing/df_generator.py
+++ processing/df_generator.py
@@ -3,8 +3,16 @@ import pandas as pd
 
 class DataframeGenerator:
     def __init__(self, csv_file: str, header: bool=None, encoding: str="utf_8", na_values: str | int | float='?'):
+        """Initializes a dataframe with certain fixes applied before returning it to the user.
+
+        Args:
+            csv_file (str): the path to the data csv file
+            header (bool, optional): whether the csv contains headers or not. Defaults to None.
+            encoding (str, optional): csv file's encoding type. Defaults to "utf_8".
+            na_values (str | int | float, optional): character or number used to define a na value within the dataset. Defaults to '?'.
+        """
         # Generate dataframe from csv file
-        self.df =pd.read_csv(csv_file, header=header, encoding=encoding, na_values=na_values)
+        self.df = pd.read_csv(csv_file, header=header, encoding=encoding, na_values=na_values)
         print(f"File at {csv_file} imported successfully as dataframe of shape: {self.df.shape}")
         # Clean possible na values
         for column in self.df.columns:
